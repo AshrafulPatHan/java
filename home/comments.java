225b2d122b79b886f7f058e636028539ed0a09dd
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2026-02-17 12:19:23 +0600

    start larning java and now i know about it

---
+++ home/comments.java
@@ -0,0 +1,9 @@
+public class comments{
+	// comment method 1
+	/*
+		comment method 2
+	*/
+	public static void main(String []args){
+		System.out.println("for output comment type // or /**/");
+	}
+}
---
+++ home/variables.java
@@ -0,0 +1,22 @@
+public class variables{
+	// add variables
+	/*
+	method :
+		type variableName = value;
+	*/
+	public static void main(String []args){
+		// my info
+		String FirstName = "Ashraful";
+		String LastName = "Pathan";
+		String name = FirstName + " " + LastName;
+		int old = 18;
+
+		System.out.println("name : "+name);
+		System.out.println("Aeg : "+old);
+
+		// math
+		int x,y,z;
+		x = y = z = 50;
+		System.out.println(x + y + z);
+	};
+}
